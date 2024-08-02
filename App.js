af42309734c68a8e1688e75ec65d485097efdc8c
tyagi.prajjwal <tyagi.prajjwal@indiamart.com>
2024-08-02 12:58:40 +0530

    Changes

--- App.js
+++ App.js
@@ -1,24 +1,5 @@
-/**
- * <div id="parent">
- *      </div id="child">
- *             <h1>
- *              I am h1 tag
- *             </h1>
- *             <h2>
- *               I am h2 tag
- *             </h2>
- *      </div>
- *      </div id="child2">
- *             <h1>
- *              I am h1 tag
- *             </h1>
- *             <h2>
- *               I am h2 tag
- *             </h2>
- *      </div>
- * </div>
- * ReactElement(Object) => HTML(Browser Understands)
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",{id: "parent"},[
@@ -36,7 +17,7 @@ const parent = React.createElement(
 
 //JSX
 
-console.log(parent) //Object
+console.log(parent); //Object
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
